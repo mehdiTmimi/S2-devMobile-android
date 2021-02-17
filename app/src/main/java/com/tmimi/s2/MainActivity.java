9855727173f66e422f80ba823f8247b4778685ac
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2021-02-17 12:44:15 +0100

    Initial commit

---
+++ app/src/main/java/com/tmimi/s2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tmimi.s2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
